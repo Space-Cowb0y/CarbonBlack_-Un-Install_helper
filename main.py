900599fee2dd843c3affaf5cb459ff24b9f5ce47
Bruno Carvalho <34915606+Space-Cowb0y@users.noreply.github.com>
2023-10-21 23:24:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 import subprocess
 import base64
 import time
-import glob
 import pyuac
-import shutil
-from getpass import getpass
+from multiprocessing import Process
+from pwinput import pwinput
 from pathlib import Path
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
@@ -13,15 +12,19 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
 uName = os.environ["username"]
-bit9_st = 'C:\\Users\\Public\\bit9.txt'
-bit9_id = 'C:\\Program Files (x86)\\Bit9\\Parity Agent'
+bit9_id = r'C:\Program Files (x86)\Bit9\Parity Agent'
+bit9_st = os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\logs\\bit9.log'
+bit9_uninstall = os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\logs\\uninstall.log'
+bit9_install = os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\logs\\install.log'
+bit9_proxy = os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\logs\\proxy.log'
+d3s_fx0 = os.environ["userprofile"]+'\\Desktop\\CB_Maintenance'
 
 #limpa tela
 def cl3_Ax0():
     os.system("cls")
 #Encrypt   
 def g3n_Ax0(Nill):
-    salt = b'<salt aqui>'
+    salt = b'<salt>'
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256,
         iterations=100000,
@@ -45,18 +48,18 @@ def rFF_Mx0(f_Mx0):
     return f3nc_Mx0
 
 def s3P_Ax0():
-    Nill = b'<secret aqui>'
+    Nill = b'<Sua senha aqui>'
     return Nill
 
-def g3t_Ax0(fP_Ax0):
+def g3t_Ax0():
     Nill = s3P_Ax0()
-    fP_Ax0 = fP_Ax0 + '\\<nome do arquivo contendo a senha criptografada>.dll'
+    fP_Ax0 = d3s_fx0 + '\\<arquivo>'
     e3n_Ax0 = rFF_Mx0(fP_Ax0)
     d3C_Ax0 = d3c_Ax0(e3n_Ax0, Nill)
     return d3C_Ax0
 #verifica versão
-def ch_x0(p4x_fx0):
-    ar_fx9(p4x_fx0)
+def ch_x0():
+    ar_fx9()
     with open(bit9_st, "r") as f_x0:
         f_x0.seek(0)
         lines = f_x0.read()
@@ -93,72 +96,74 @@ def ch_x0(p4x_fx0):
             return '{DA971CA3-73AA-4A57-AFB4-8155E72CEB96}'
         else:
             return "Versão desconhecida"
-    
-def r_Ax0(p4x_fx0):
+#remove bit9   
+def r_Ax0():
     cl3_Ax0()
     print('== == == == == == == == == == == == == == == == ==')
     print(' == == == == == REMOVER BIT9 == == == == == == == ')
     print('== == == == == == == == == == == == == == == == ==')
     try:
         os.chdir(bit9_id)
-    except FileNotFoundError:
+    except OSError:
         print("O Bit9 não está instalado.")
         input("Pressione Enter para continuar...")
         return 0
-    sh_tx56=getpass("Por favor, insira a senha Master para realizar a desinstalação: ",'*')
-    if sh_tx56 == p4x_fx0:
-        os.chdir(bit9_id)
-        t3m_fx0(p4x_fx0)
-        swe_fx0 = ch_x0(p4x_fx0)
-        os.chdir(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents')
-        subprocess.run(['AgentUninstallUtility.exe', '-password', p4x_fx0, '-logfile', os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\uninstallLog.txt', "-uninstall", '/s', '/y'])
-        subprocess.run(["cmd.exe", "/C", "del", "C:\\Program Files (x86)\\Bit9", "/f", "/Q"])
-        subprocess.run(["cmd.exe", "/C", "del", "C:\\Windows\\System32\\drivers\\Parity.sys", '/f'])
-        subprocess.run(["cmd.exe", "/C", "del", "C:\\Windows\\System32\\DRVSTORE\\Parity*.*", '/f'])
-        print('')
-        print('Aguarde, estamos finalizando a desinstalação')
-        subprocess.run(['msiexec.exe', '/x', swe_fx0, '/qn', 'FORCE=1'])
-        subprocess.run(['AgentUninstallUtility.exe', '-password', p4x_fx0, "-uninstall", '/s', '/y'])
-        try:
+    if os.path.exists(bit9_id+'\\DasCLI.exe'):
+        #evita que qualquer um desisntale 
+        sh_tx56=pwinput(prompt="Por favor, insira a senha Master para realizar a desinstalação: ", mask='*')
+        if sh_tx56 == g3t_Ax0():
+            print("Iniciando Processo...")
+            t3m_fx0()
+            swe_fx0 = ch_x0()
+            os.chdir(d3s_fx0+'\\agents')
+            subprocess.run(['AgentUninstallUtility.exe', '-password', g3t_Ax0(), "-uninstall", '/s', '/y'],stdout=open(bit9_uninstall, "a"))
+            subprocess.run(["cmd.exe", "/C", "del", "C:\\Program Files (x86)\\Bit9", "/f", "/Q"],stdout=open(bit9_uninstall, "a"),stderr=open(bit9_uninstall,"a"))
+            subprocess.run(["cmd.exe", "/C", "del", "C:\\Windows\\System32\\drivers\\Parity.sys", '/f'],stdout=open(bit9_uninstall, "a"),stderr=open(bit9_uninstall,"a"))
+            subprocess.run(["cmd.exe", "/C", "del", "C:\\Windows\\System32\\DRVSTORE\\Parity*.*", '/f'],stdout=open(bit9_uninstall, "a"),stderr=open(bit9_uninstall,"a"))
+            print('')
+            print('Aguarde, estamos finalizando a desinstalação')
+            print('')
+            subprocess.run(['msiexec.exe', '/x', swe_fx0, '/qn', 'FORCE=1'],stdout=open(bit9_uninstall, "a"))
+            print('')
+            print('Mais alguns instantes...')
+            print('')
+            subprocess.run(['AgentUninstallUtility.exe', '-password', g3t_Ax0(), "-uninstall", '/s', '/y'],stdout=open(bit9_uninstall, "a"))
             print("Finalizando processos msiexec")
-            subprocess.run(['taskkill', '/IM', 'msiexec.exe', '/F'])
-        except:
-            print("Continuando desinstalação...")
-        print("\n\nNão é necessário o reinicio do servidor...")
-        restart = input("Gostaria de reiniciar o servidor agora ? ( S / N (Enter)):")
-        if restart == 's' or restart == 'S':
-            os.system("shutdown /r /t 1")
+            subprocess.run(['taskkill', '/IM', 'msiexec.exe', '/F'],stdout=open(bit9_uninstall, "a"),stderr=open(bit9_uninstall,"a"))
+            print("\n\nNão é necessário o reinicio do servidor...")
+            restart = input("Gostaria de reiniciar o servidor agora ? ( S / N (Enter)):")
+            if restart == 's' or restart == 'S':
+                os.system("shutdown /r /t 1")
         else:
-            return 0
+            cl3_Ax0()
+            input("Senha incorreta! Aperte enter para voltar ao Menu.")
     else:
-        input("Senha incorreta! Aperte enter para voltar ao Menu")
+        print("O Bit9 não está instalado.")
+        input("Pressione Enter para continuar...")
     return 0
-
-
-def in5_Ax0(p4x_fx0):
+#instala bit9
+def in5_Ax0():
     cl3_Ax0()
     print('== == == == == == == == == == == == == == == == ==')
     print('== == == == == INSTALAR BIT9 == == == == == == == ')
     print('== == == == == == == == == == == == == == == == ==')
-    if os.path.exists(r"C:\Program Files (x86)\Bit9\Parity Agent"):
-        print("Bit9 já está instalado!")
-        input("Pressione Enter para continuar...") 
-    else:
-        #os.chdir(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents')
-        print("Finalizando processos msiexec abertos")
-        try:
-            subprocess.run(['taskkill', '/IM', 'msiexec.exe', '/F'])
-        except ValueError:
-            print("Continuando instalação...")
-        print('Executando instalador, por favor aguarde...')
-        subprocess.run(['msiexec.exe', '/i', os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\fisbr-monitor.msi', '/qn', '/L*V', os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\monitor_install.log'])
-        g4T_Ax0(p4x_fx0)
-        print("Executado com sucesso.")
+    try:
+        if os.path.exists(bit9_id+'\\DasCLI.exe'):
+            print("Bit9 já está instalado!")
+            input("Pressione Enter para continuar...") 
+        else:
+            print("Finalizando processos msiexec abertos...")
+            subprocess.run(['taskkill', '/IM', 'msiexec.exe', '/F'], stdout=open(bit9_install, "a"),stderr=open(bit9_install,"a"))
+            print('Executando instalador, por favor aguarde...')
+            subprocess.run(['msiexec.exe', '/i', os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\fisbr-monitor.msi', '/qn'],stdout=open(bit9_install, "a"))
+            g4T_Ax0()
+            print("Executado com sucesso.")
+            input("Pressione Enter para continuar...")
+    except OSError:
+        print("Houve um problema ao tentar instalar, tente novamente.")
         input("Pressione Enter para continuar...")
-
     return 0
-
-
+#coletar status
 def r3P_Fx0():
     cl3_Ax0()
     print('== == == == == == == == == == == == == == == == ==')
@@ -166,121 +171,138 @@ def r3P_Fx0():
     print('== == == == == == == == == == == == == == == == ==')
     try:
         os.chdir(bit9_id)
+        subprocess.run(['DasCLI.exe', 'status'], stdout=open(bit9_st, "w"))
+        with open(bit9_st, "r") as f_x2:
+            f_x2.seek(0)
+            lines = f_x2.read()
+            f_x2.close()
+            print(lines)
     except FileNotFoundError:
         print("O Bit9 não está instalado.")
         input("Pressione Enter para continuar...")
         return 0
-    os.chdir(bit9_id)
-    subprocess.run(['DasCLI.exe', 'status'], stdout=open(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\bit9.txt', "w"))
-    with open(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\bit9.txt', "r") as f_x2:
-        f_x2.seek(0)
-        lines = f_x2.read()
-        f_x2.close()
-        print(lines)
+    except OSError:
+        print("O Dascli não pode ser encontrado, tente novamente.")
+        input("Pressione Enter para continuar...")
+        return 0
     print('')
     print("Executado com sucesso.")
     input("Pressione Enter para continuar...")
     return 0
-
+#retorna versão
 def r3P_Fx1():
     cl3_Ax0()
     try:
         os.chdir(bit9_id)
-    except FileNotFoundError:
+    except OSError:
         return 'Versão do Carbon Black não disponível'
-    os.chdir(bit9_id)
-    subprocess.run(['DasCLI.exe', 'status'], stdout=open(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\bit9.txt', "w"))
-    with open(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\bit9.txt', "r") as f_x0:
-        f_x0.seek(0)
-        lines = f_x0.read()
-        f_x0.close()
-        if '    Agent:      8.9' in lines:
-            return '8.9'
-        elif '    Agent:      8.8' in lines:
-            return '8.8'
-        elif '    Agent:      8.7.8' in lines:
-            return '8.7.8'
-        elif '    Agent:      8.7.6' in lines:
-            return '8.7.6' 
-        elif '    Agent:      8.7.5' in lines:
-            return '8.7.5' 
-        elif '    Agent:      8.7.4' in lines:
-            return '8.7.4' 
-        elif '    Agent:      8.7.2' in lines:
-            return '8.7.2' 
-        elif '    Agent:      8.7.0' in lines:
-            return '8.7.0'
-        elif '    Agent:      8.6' in lines:
-            return '8.6'
-        elif '    Agent:      8.5' in lines:
-            return '8.5'
-        elif '    Agent:      8.4' in lines:
-            return '8.4'
-        elif '    Agent:      8.3' in lines:
-            return '8.3'
-        elif '    Agent:      8.2' in lines:
-            return '8.2'
-        elif '    Agent:      8.1' in lines:
-            return '8.1'
-        elif '    Agent:      8.0' in lines:
-            return '8.0'
-        else:  
-            return 'Versão do Carbon Black não suportada'
-   
-def g4T_Ax0(p4x_fx0):
-    print('== == == == == == == == == == == == == == == == ==')
-    print('== == == == == CONFIGURAÇÃO DASCLI == == == == == ')
-    print('== == == == == == == == == == == == == == == == ==')
-    print('Liberando Tamper...')
+    try:
+        subprocess.run(['DasCLI.exe', 'status'], stdout=open(bit9_st, "w"))
+        with open(bit9_st, "r") as f_x0:
+            f_x0.seek(0)
+            lines = f_x0.read()
+            f_x0.close()
+            if '    Agent:      8.9' in lines:
+                return '8.9'
+            elif '    Agent:      8.8' in lines:
+                return '8.8'
+            elif '    Agent:      8.7.8' in lines:
+                return '8.7.8'
+            elif '    Agent:      8.7.6' in lines:
+                return '8.7.6' 
+            elif '    Agent:      8.7.5' in lines:
+                return '8.7.5' 
+            elif '    Agent:      8.7.4' in lines:
+                return '8.7.4' 
+            elif '    Agent:      8.7.2' in lines:
+                return '8.7.2' 
+            elif '    Agent:      8.7.0' in lines:
+                return '8.7.0'
+            elif '    Agent:      8.6' in lines:
+                return '8.6'
+            elif '    Agent:      8.5' in lines:
+                return '8.5'
+            elif '    Agent:      8.4' in lines:
+                return '8.4'
+            elif '    Agent:      8.3' in lines:
+                return '8.3'
+            elif '    Agent:      8.2' in lines:
+                return '8.2'
+            elif '    Agent:      8.1' in lines:
+                return '8.1'
+            elif '    Agent:      8.0' in lines:
+                return '8.0'
+            else:  
+                return 'Versão do Carbon Black não disponível'
+    except OSError:
+        return 'Versão do Carbon Black não disponível'
+#configura dascli
+def g4T_Ax0():
+    print('Configurando Dascli')
     countdown(15)
+    t3m_fx1()
     print('Esperando client se conectar...')
     countdown(45)
-    os.chdir(bit9_id)
-    subprocess.run(['DasCLI.exe', 'password', p4x_fx0])
-    subprocess.run(['DasCLI.exe', 'tamperprotect', '1'])
-    subprocess.run(['DasCLI.exe', 'allowuninstall', '0'])
     return 0
-    
-def t3m_fx0(p4x_fx0):
-    os.chdir(bit9_id)
-    subprocess.run(['DasCLI.exe', 'password', p4x_fx0])
-    subprocess.run(['DasCLI.exe', 'tamperprotect', '0'])
-    subprocess.run(['DasCLI.exe', 'allowuninstall', '1'])
-    return 0
-
-def ar_fx9(p4x_fx0):
+#tira tamper
+def t3m_fx0():
     try:
         os.chdir(bit9_id)
-    except:
+        subprocess.run(['DasCLI.exe', 'password', g3t_Ax0()],stdout=open(bit9_uninstall, "a"))
+        subprocess.run(['DasCLI.exe', 'tamperprotect', '0'],stdout=open(bit9_uninstall, "a"))
+        subprocess.run(['DasCLI.exe', 'allowuninstall', '1'],stdout=open(bit9_uninstall, "a"))
+    except OSError:
+        print("Houve um problema ao tentar remover o AntiTamper")
+        input("Pressione Enter para continuar...")
+    return 0
+#coloca tamper
+def t3m_fx1():
+    try:
+        os.chdir(bit9_id)
+        subprocess.run(['DasCLI.exe', 'password', g3t_Ax0()],stdout=open(bit9_install, "a"))
+        subprocess.run(['DasCLI.exe', 'tamperprotect', '1'],stdout=open(bit9_install, "a"))
+        subprocess.run(['DasCLI.exe', 'allowuninstall', '0'],stdout=open(bit9_install, "a"))
+    except OSError:
+        print("Houve um problema com o Dascli, tente novamente após instalar")
+        input("Pressione Enter para continuar...")
+    return 0
+#seta password
+def ar_fx9():
+    try:
+        os.chdir(bit9_id)
+    except OSError:
         print("O Bit9 não está instalado.")
         input("Pressione Enter para continuar...")
-        return 0
-    os.chdir(bit9_id)
-    wd_fx0 = os.getcwd()
-    if wd_fx0 != bit9_id:
-        os.chdir(bit9_id)
-    subprocess.run(['DasCLI.exe', 'status'], stdout=open(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\bit9.txt', "w"))
-    subprocess.run(['DasCLI.exe', 'password', p4x_fx0])
+    try:
+        subprocess.run(['DasCLI.exe', 'password', g3t_Ax0()])
+    except OSError:
+        print("Houve um problema com o Dascli, tente novamente após instalar")
+        input("Pressione Enter para continuar...")
     return 0
-
+#verifica proxy
 def sh_prox():
     cl3_Ax0()
+    print('== == == == == == == == == == == == == == == == ==')
+    print('== == == == == Verificando Proxy == == == == == ==')
+    print('== == == == == == == == == == == == == == == == ==')
     print("Verificando proxy...")
-    subprocess.run(['netsh.exe', 'winhttp', 'show', 'proxy'], stdout=open(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\proxy.txt', "w"))
-    with open(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\proxy.txt', "r") as f_x0:
+    subprocess.run(['netsh.exe', 'winhttp', 'show', 'proxy'], stdout=open(bit9_proxy, "w"))
+    with open(bit9_proxy, "r") as f_x0:
         f_x0.seek(0)
         lines = f_x0.read()
         f_x0.close()
         if 'Direct access (no proxy server).' in lines:
             print('')
-            input("O proxy está configurado! Aperte ENTER para voltar ao menu")
+            print("O proxy está configurado!")
+            print('Continuando em: ')
+            countdown(5)
             return 0
         print("Proxy não configurado")
         print("Configurando proxy...")
         subprocess.run(['netsh.exe', 'winhttp', 'reset', 'proxy'])
         input('Proxy configurado! Aperte ENTER para voltar ao menu')
         return 0      
-
+#faz o countdown
 def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
@@ -289,26 +311,24 @@ def countdown(t):
         time.sleep(1)
         t -= 1
     return 0
-
+#banner
 def inf_Ax0(version):
     print('=========================================================================')
-
     print("Computador:", os.environ["computername"],
         " Usuario:", os.environ["username"], " cdw:", os.getcwd())
-    print(" Versão do Bit9:", version)
-
+    print("\nVersão do Bit9:", version)
     print('=========================================================================')
-
+#verifica se é adm
 if not pyuac.isUserAdmin():
     print("Abrindo nova instância como Admin!")
+    print("Caso isso não resolva, executar como administrador a partir de um CMD!")
     pyuac.runAsAdmin()
     #for f in os.listdir(os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\'):
     #    if 'Maintenance.exe' in f:
     #       subprocess.run(['cmd.exe', '/C', 'del', os.environ["userprofile"]+'\\Desktop\\CB_Maintenance\\agents\\Maintenance.exe'])
+#menu
 else:
-    d3s_fx0 = os.environ["userprofile"]+'\\Desktop\\CB_Maintenance'
     os.chdir(d3s_fx0)
-    p4x_fx0 = g3t_Ax0(d3s_fx0)
     menu = {}
     menu['1'] = "Validação/Correção proxy"
     menu['2'] = "Remover Bit9"
@@ -317,7 +337,6 @@ else:
     menu['7'] = "Sair"
     while True:
         options = menu.keys()
-        #options.sort()
         vers = r3P_Fx1()
         cl3_Ax0()
         inf_Ax0(vers)
@@ -330,16 +349,13 @@ else:
             sh_prox()
             print("Executando desinstalação...")
             countdown(5)
-            r_Ax0(p4x_fx0)
+            r_Ax0()
         elif selection == '3':
-            in5_Ax0(p4x_fx0)
+            in5_Ax0()
         elif selection == '4':
             r3P_Fx0()
         elif selection == '7':
             break
         else:
             input("Opção inexistente! Aperte qualquer tecla para voltar ao menu")
-
-#for f in os.listdir(os.environ["userprofile"]+'\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'):
-#        if 'Maintenance.exe' in f:
-#            subprocess.run(['cmd.exe', '/C', 'del', os.environ["userprofile"]+'\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Maintenance.exe'])
+cl3_Ax0()
